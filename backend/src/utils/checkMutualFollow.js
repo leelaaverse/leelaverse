e8f33a071e7b6d47d563c61923b0001ee6523134
Nikhil Singh <nikhilsingh.cc@gmail.com>
2026-01-06 08:34:55 -0800

    fix: use shared prisma instance in checkMutualFollow

--- backend/src/utils/checkMutualFollow.js
+++ backend/src/utils/checkMutualFollow.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const prisma = require('../config/prisma');
 
 /**
  * Check if two users follow each other (mutual following)
